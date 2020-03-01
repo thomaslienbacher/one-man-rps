c9a82629a00ee6f1bc5503804103e34a29238408
thomaslienbacher <lienbacher.tom@gmail.com>
2020-03-01 14:55:47 +0100

    Finished final UI and made improvements to dev UI
    The final UI is finished and displays the title, the move to counter the prediction and a percentage of what was predicted. The dev UI also displays an error image if nothing was properly detected.

--- prototype-scripts/src/rps_robot.py
+++ prototype-scripts/src/rps_robot.py
@@ -1,15 +1,14 @@
 """
-Dieses Script nimmt laded ein vortrainiertes Neural Netzwerk, und testet es mit
-Echzeit Bildern von der Kamera. Das Ergebnis und die Bilder werden grafisch dargestellt.
+Dieses Script ist der finale Schere Stein Papier Roboter. Es liest den Kamerafeed, erkennt die Geste
+und stellt das Gegenzeichen grafisch dar.
 """
 
-from tensorflow.keras.models import model_from_json, load_model
+from tensorflow.keras.models import model_from_json
 import os
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2 as cv
-import matplotlib.pyplot as plt
 from utils import *
 import numpy as np
 from tkinter import *
@@ -27,30 +26,23 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=IMG_NET_SIZE)
 time.sleep(0.5)
 
-path = os.path.join(DATADIR, "v5_model_architecture.json")
+path = os.path.join(DATADIR, "v6_model_architecture.json")
 with open(path, "r") as f:
     model = model_from_json(f.read())
-path = os.path.join(DATADIR, "v5_model_weights.h5")
+path = os.path.join(DATADIR, "v6_model_weights.h5")
 model.load_weights(path)
 print("Loaded model!")
 
 # Window created with tkinter
 window = Tk()
 window.attributes("-fullscreen", True)
+window.configure(background="white", cursor="none")
 window.update()
-image_size = window.winfo_width() // 2 - PADDING * 2 - BORDER * 2
+image_size = int(window.winfo_width() * 0.66) - PADDING * 2 - BORDER * 2
 
 Style().configure("TFrame", background="white")
 Style().configure("CustomPanel.TLabel", borderwidth=BORDER, relief="flat", background="black")
 Style().configure("CustomLabel.TLabel", background="white", font="jetbrainsmono 32")
-
-
-def key_up(e):
-    if e.keysym == 'Escape':
-        sys.exit(0)
-
-
-window.bind('<KeyRelease>', key_up)
 
 # pre loaded images
 cv_img = cv.cvtColor(cv.imread("rock.png"), cv.COLOR_BGR2RGB)
@@ -69,23 +61,19 @@ cv_img = cv.cvtColor(cv.imread("blank.png"), cv.COLOR_BGR2RGB)
 cv_img = cv.resize(cv_img, (image_size, image_size))
 photo_empty = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
 
-cv_img = cv.cvtColor(cv.imread("error.png"), cv.COLOR_BGR2RGB)
-cv_img = cv.resize(cv_img, (image_size, image_size))
-photo_error = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
-
 # tk panels
 panel_frame = Frame(window)
 label_frame = Frame(window)
 
 panel_gesture = Label(panel_frame, image=photo_empty, style="CustomPanel.TLabel")
-panel_gesture.pack(side="left", fill="x", expand=False, padx=(PADDING, PADDING), pady=(PADDING, PADDING))
-panel_cam = Label(panel_frame, image=photo_empty, style="CustomPanel.TLabel")
-panel_cam.pack(side="right", fill="x", expand=False, padx=(PADDING, PADDING), pady=(PADDING, PADDING))
+panel_gesture.pack(side="bottom", fill="x", expand=False, padx=(PADDING, PADDING), pady=(PADDING, PADDING))
+label_title = Label(panel_frame, text="Schere Stein Papier - Roboter", style="CustomLabel.TLabel")
+label_title.pack(side="top", fill="y", anchor="n", expand=False, pady=(PADDING, PADDING))
 panel_frame.pack(side="top")
 panel_frame.update()
 
-label = Label(label_frame, text="sandbox-text", style="CustomLabel.TLabel")
-label.pack(side="bottom", fill="y", anchor="n", expand=TRUE)
+label = Label(label_frame, text="", style="CustomLabel.TLabel")
+label.pack(side="bottom", fill="y", anchor="n", expand=True)
 label_frame.pack(side="bottom", fill="both", anchor="center", expand=True)
 label_frame.update()
 
@@ -103,26 +91,22 @@ for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=
     prediction = prediction[0]
     highest = -1
     if prediction[0] < THRESHOLD and prediction[1] < THRESHOLD and prediction[2] < THRESHOLD and prediction[3] < THRESHOLD:
-        prediction_text = "E: {:>5.1f}  P: {:>5.1f}  R: {:>5.1f}  S: {:>5.1f} {:_>9}".format(
-            prediction[0] * 100, prediction[1] * 100,
-            prediction[2] * 100, prediction[3] * 100, "--")
-        panel_gesture.configure(image=photo_error)
+        prediction_text = "{:>6} zu {:>5.1f}% erkannt".format("", 0)
+        panel_gesture.configure(image=photo_empty)
     else:
         CATEGORIES = ["EMPTY", "PAPER", "ROCK", "SCISSORS"]
+        CATEGORIES_DE = ["Leer", "Papier", "Stein", "Schere"]
         out = np.argmax(prediction)
 
         if isinstance(out, list):
-            prediction_text = "E: {:>5.1f}  P: {:>5.1f}  R: {:>5.1f}  S: {:>5.1f} {:>8}".format(
-                prediction[0] * 100, prediction[1] * 100,
-                prediction[2] * 100, prediction[3] * 100, CATEGORIES[out[0]])
+            prediction_text = "{:>6} zu {:>5.1f}% erkannt".format(
+                CATEGORIES_DE[out[0]], prediction[out[0]] * 100)
             highest = out[0]
         else:
-            prediction_text = "E: {:>5.1f}  P: {:>5.1f}  R: {:>5.1f}  S: {:>5.1f} {:>8}".format(
-                prediction[0] * 100, prediction[1] * 100,
-                prediction[2] * 100, prediction[3] * 100, CATEGORIES[out])
+            prediction_text = "{:>6} zu {:>5.1f}% erkannt".format(
+                CATEGORIES_DE[out], prediction[out] * 100)
             highest = out
 
-    print(prediction_text)
     label.configure(text=prediction_text)
 
     if highest == 0:
@@ -136,9 +120,6 @@ for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=
 
     if highest == 3:
         panel_gesture.configure(image=photo_rock)
-
-    cam_photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv.resize(image * 255, (image_size, image_size))))
-    panel_cam.configure(image=cam_photo)
 
     rawCapture.truncate(0)
     window.update()
